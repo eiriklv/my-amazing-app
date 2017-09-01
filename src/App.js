d5d1fcee7e8dacc01c92f4c32c975f30f5b6b04d
Eirik Vullum <evullum@gmail.com>
2017-09-01 14:52:13 +0200

    add text

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ class App extends Component {
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
+          <h2>I have been updated</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
